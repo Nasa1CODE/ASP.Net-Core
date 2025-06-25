5acb229899206bee9d7a680d488da751f2cef032
ManhDev <97645883+Nasa1CODE@users.noreply.github.com>
2025-06-25 23:33:14 +0700

    Project configuration

---
+++ CompanyEmployees/CompanyEmployees/Program.cs
@@ -0,0 +1,34 @@
+﻿using CompanyEmployees.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();//sẽ thêm phần mềm trung gian để sử dụng HSTS, phần mềm này sẽ thêm tiêu đề Strict-Transport-Security
+
+app.UseHttpsRedirection();//được sử dụng để thêm phần mềm trung gian cho việc chuyển hướng từ HTTP sang HTTPS
+app.UseStaticFiles();//cho phép sử dụng các tệp tĩnh cho yêu cầu
+app.UseForwardedHeaders(new ForwardedHeadersOptions // sẽ chuyển tiếp các tiêu đề proxy đến yêu cầu hiện tại
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
